5ccd9139bd783b453fb6093198c835287c06d6fb
sjbaek <sjbaek@gmail.com>
2016-05-11 16:50:05 -0700

    feat: change enforce_deadline => True and observe how the code output changes

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -28,13 +28,12 @@ class LearningAgent(Agent):
         #print self.prev_waypoint, self.next_waypoint
 
         # TODO: Update state
-        # self.state = (inputs['light'], self.next_waypoint, deadline)
+        self.state = (inputs['light'], self.next_waypoint, deadline)
 
         # TODO: Select action according to your policy
         
         # action = self.prev_waypoint
         action = random.choice([None, 'forward','left','right'])
-
         # action = self.next_waypoint
 
         # Execute action and get reward
@@ -53,10 +52,10 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # set agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 	
 	# Now simulate it
-    sim = Simulator(e, update_delay=0.1)  # reduce update_delay to speed up simulation
+    sim = Simulator(e, update_delay=1.0/10.)  # reduce update_delay to speed up simulation
     sim.run(n_trials=10)  # press Esc or close pygame window to quit
 
 if __name__ == '__main__':
