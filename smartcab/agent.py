07ce06add862d56712400572684a99b84973ba05
sjbaek <sjbaek@gmail.com>
2016-05-13 16:29:16 -0700

    feat: Begin implementing Q-learning

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -15,6 +15,10 @@ class LearningAgent(Agent):
         # initialize Q-matrix
         self.Qmat = {}
         
+        # initialize other variables
+        self.gamma = 0.0
+        self.alpha = 0.5
+
         # This function reaches at the very beginning of the script
 
     def reset(self, destination=None):
@@ -36,7 +40,8 @@ class LearningAgent(Agent):
         #print self.prev_waypoint, self.next_waypoint
 
         # TODO: Update state
-        self.state = (inputs['light'], self.next_waypoint, deadline)
+        # self.state = (inputs['light'], self.next_waypoint, deadline)
+        self.state = (inputs['light'], self.next_waypoint)
         self.serial += 1
         
         # To see if serial number continues after the agent reaches destination.
@@ -44,23 +49,39 @@ class LearningAgent(Agent):
 
         # TODO: Select action according to your policy
         if self.state in self.Qmat.keys():
-    	# print self.state, self.Qmat.keys()
-    	    print "found it"
-    	    action = self.next_waypoint
+    	    print "-------------------------------------- yes"
+    	    action_key_value = self.Qmat[self.state]  # this shows key/value of selected state in Qmat
+    	    action = max(action_key_value, key = action_key_value.get) # select action of highest probability
+    	    # action = self.next_waypoint
     	else:
-    		print "not there yet"
+    		print "---------------------- no"
+    		#self.Qmat[self.state] = 'dummy'
+    		self.Qmat[self.state] = {None:0, 'forward':0, 'left':0, 'right':0}
+
+    		# random action, when landed on a 'new' state
     		action = random.choice([None, 'forward','left','right'])
         
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # TODO: Learn policy based on state, action, reward
-  
-        if self.state not in self.Qmat.keys():# self.next_waypoint is None:
-        	self.Qmat[self.state] = 'dummy'
+
+        # Q(s',a') based on selected action
+        # new_state = (inputs['light'],action,deadline-1)
+        new_state = (inputs['light'],action)
+        if new_state in self.Qmat.keys():
+        	Q_nextT = max(self.Qmat[new_state].values())
+        else:
+        	Q_nextT = 0.
+        
+        Qhat = (1-self.alpha)*self.Qmat[self.state][action] + (self.alpha*(reward+self.gamma*Q_nextT))
+        self.Qmat[self.state][action] = Qhat
+        print "Qhat={}, Q_nextT={}, action={}, NWP={}".format(Qhat,Q_nextT,action,self.next_waypoint)
+
+        	
  
 
-        # print "next_waypoint={},action = {},inputs={}".format(self.next_waypoint, action ,inputs)
+        #print "next_waypoint={},action = {},inputs={}".format(self.next_waypoint, action ,inputs)
         # print "next_waypoint={},A = {}, inputs={},R={}".format(self.next_waypoint, action,inputs ,reward)
         # print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
@@ -71,7 +92,7 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # set agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 	
 	# Now simulate it
     sim = Simulator(e, update_delay=1.0/10.)  # reduce update_delay to speed up simulation
